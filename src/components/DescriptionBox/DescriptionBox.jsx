79a3079adfb4da533f9a47aaea69040a4ff68550
Victor Chiagozie Chidozie <chidoziev742@gmail.com>
2025-10-25 13:14:30 +0100

    Fix DescriptionBox component and CSS styling

--- src/components/DescriptionBox/DescriptionBox.jsx
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 import "./DescriptionBox.css";
 
-const DescriptionBox=()=>{
-    return(
-        <div className="descIrptionbox">
-<div className="descIrptionbox-navigator">
-    <div className="descIrptionbox-nav-box">
-        Description
-    </div>
-    <div className="descIrptionbox-nav-box-fade">
-        Reviews (122)
-    </div>
-    <div className="descriptionbox-description">
-        <p>An e-commerce website is an online platform where businesses sell products or services directly to consumers over the internet. It functions as a digital storefront
-             </p>
-             <p>🛒 Browse products or services through searchable catalogs
-
-🧺 Add items to a shopping cart and manage selections
-
-💳 Make secure payments via integrated payment gateways
-
-📦 Track orders and manage deliveries without visiting a physical store </p>
-    </div>
-</div>
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box descriptionbox-nav-box-fade">
+          Reviews (122)
         </div>
-    )
-}
+      </div>
 
-export default DescriptionBox
+      <div className="descriptionbox-content">
+        <p>
+          An e-commerce website is an online platform where businesses sell
+          products or services directly to consumers over the internet. It
+          functions as a digital storefront.
+        </p>
+        <p>
+          🛒 Browse products or services through searchable catalogs
+          <br />
+          🧺 Add items to a shopping cart and manage selections
+          <br />
+          💳 Make secure payments via integrated payment gateways
+          <br />
+          📦 Track orders and manage deliveries without visiting a physical
+          store
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
